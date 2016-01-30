08618aeb18bda3c50e09dfcdb0aeea0b99265f77
zouchao2010 <nlzc@163.com>
2016-01-31 01:31:58 +0800

    default

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	_ "cakeshop-go/routers"
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	beego.Run()
+}
+
---
+++ routers/router.go
@@ -0,0 +1,11 @@
+package routers
+
+import (
+	"cakeshop-go/controllers"
+	"github.com/astaxie/beego"
+)
+
+func init() {
+//    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+}
