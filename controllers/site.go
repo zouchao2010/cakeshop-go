0f17659194ca836d373d3ed87afdafcb400fa068
zouchao2010 <nlzc@163.com>
2016-01-31 22:46:37 +0800

    site

--- controllers/site.go
+++ controllers/site.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"cakeshop-go/models"
+	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	"strings"
 )
 
 type MainController struct {
@@ -9,8 +13,28 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-//	c.Data["Website"] = "beego.me"
-//	c.Data["Email"] = "astaxie@gmail.com"
-	c.Data["user"] = nil
+	uid := c.GetString("uid")
+	fmt.Println(uid)
+	o := orm.NewOrm()
+	ads := make([]*models.Ad, 0)
+	shops := make([]*models.Shop, 0)
+	newest := make([]*models.Shop, 0)
+	recomm := make([]*models.Shop, 0)
+	o.QueryTable("ad").Limit(6).All(&ads)
+	o.QueryTable("shop").Exclude("cid", 2).Exclude("status",9).OrderBy("-views").Limit(6).All(&shops,"name","ename","cover","price")
+	for _, shop := range(shops){
+		shop.Price =  strings.Split(shop.Price, "~")[0]
+		newest = append(newest, shop)
+	}
+	shops = shops[:0]
+	o.QueryTable("shop").Filter("status",1).Limit(6).All(&shops,"name","ename","cover","price")
+	for _, shop := range(shops){
+		shop.Price =  strings.Split(shop.Price, "~")[0]
+		recomm = append(recomm, shop)
+	}
+	fmt.Println(len(ads))
+	c.Data["ads"] = ads
+	c.Data["newest"] = newest
+	c.Data["recomm"] = recomm
 	c.TplName = "site/index.html"
 }
--- main.go
+++ main.go
@@ -1,11 +1,43 @@
 package main
 
 import (
+	_ "cakeshop-go/models"
 	_ "cakeshop-go/routers"
+//	 "cakeshop-go/models"
+	"fmt"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+//	"io"
+
 )
 
+func init() {
+//	var w io.Writer
+	orm.Debug = true
+//	orm.DebugLog = orm.NewLog(w)
+	orm.RegisterDriver("mysql", orm.DRMySQL)
+	orm.RegisterDataBase("default", "mysql", "root:root@tcp(192.168.147.151:3306)/database?charset=utf8", 30, 30)
+	fmt.Println("main.init..........")
+}
 func main() {
+	beego.SetStaticPath("/upload", "upload")
+
+//	beego.BConfig.WebConfig.TemplateLeft="{%"
+//	beego.BConfig.WebConfig.TemplateRight="%}"
+
 	beego.Run()
+
+	//	o := orm.NewOrm()
+	//	o.Using("default") // 默认使用 default，你可以指定为其他数据库
+	//	qs := o.QueryTable("user")
+	//
+	//	users := make([]*models.User,0)
+	//	_, _ = qs.All(&users)
+	//	fmt.Println(users)
+	//	for _, user := range users {
+	//		fmt.Println(user.Mobile)
+	//
+	//	}
 }
 
